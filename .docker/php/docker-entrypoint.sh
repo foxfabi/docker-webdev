c8cfc99ed8088e1da95574288ded516bf66ff1b9
foxfabi <fd@fabforge.ch>
2021-06-02 14:05:30 +0200

    Start swoole server, keep development server for later testing

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-echo ' Starting PHP Server'
-/usr/local/bin/php -S 0.0.0.0:7409 -t /var/www/backend/public/ &
+#echo ' Starting PHP Server'
+#/usr/local/bin/php -S 0.0.0.0:7409 -t /var/www/backend/public/ &
+
+echo ' Starting PHP Swoole Server'
+/usr/local/bin/php /var/www/backend/public/server.php &
+
 # Run the given command or start defaults
 if [ $# -gt 0 ];then
     # If we passed a command, run it

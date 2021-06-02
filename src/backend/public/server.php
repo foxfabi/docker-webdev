bb1dba1a4c3f0ed5f25872db385d2ea3fc73e991
foxfabi <fd@fabforge.ch>
2021-06-02 14:05:53 +0200

    Fixed Undefined variable: port

--- src/backend/public/server.php
+++ src/backend/public/server.php
@@ -7,7 +7,7 @@ $port = 7409;
 $server = new Server("0.0.0.0", $port);
 
 $server->on("start", function (Swoole\Http\Server $server) {
-    sprintf("Swoole http server is started at http://0.0.0.0:%d\n", $port);
+    sprintf("Swoole http server is started at http://0.0.0.0:%d\n", $server->port);
 });
 
 $server->on("request", function (Swoole\Http\Request $request, Swoole\Http\Response $response) {

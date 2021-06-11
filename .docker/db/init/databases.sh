05de9bc961b5e876266527f6daddf70a07a792fd
foxfabi <fd@fabforge.ch>
2021-06-11 14:07:58 +0200

    Added description while it runs once. Added readonly user for grafana

--- .docker/db/init/databases.sh
+++ .docker/db/init/databases.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# If you start the mariadb container instance with a data directory that already
+# contains a database (specifically, a mysql subdirectory), this script will be ignored
+
 set -eo pipefail
 
 _create_database() {
@@ -19,6 +22,12 @@ for DATABASE_NAME in $DATABASE_APP $DATABASE_GITEA; do
   _create_database $DATABASE_NAME
 done
 
+echo "Grant permissions"
 # Grant permissions to access and use the MySQL server
 mysql -u root --password=${MARIADB_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
 mysql -u root --password=${MARIADB_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+
+echo "Adding grafana user with restricted permissions"
+mysql -u root --password=${MARIADB_PASSWORD} -e "CREATE USER IF NOT EXISTS 'grafanaReader' IDENTIFIED BY 'grafana';"
+mysql -u root --password=${MARIADB_PASSWORD} -e "GRANT SELECT ON *.* TO 'grafanaReader'@'%' IDENTIFIED BY 'grafana';"
+mysql -u root --password=${MARIADB_PASSWORD} -e "GRANT SELECT ON *.* TO 'grafanaReader'@'localhost' IDENTIFIED BY 'grafana';"
